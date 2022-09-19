a3fbb8db8ee319c679afd2d22e0b5f091e88525b
Akashagrawal <akash242018@gmail.com>
2022-09-20 01:31:08 +0530

    simple routing setup set

---
+++ src/components/Bookmark.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Bookmark() {
+  return (
+    <div>Bookmark component</div>
+  )
+}
+
+export default Bookmark
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+export default function Navbar() {
+  return (
+    <nav style={{display:'flex',justifyContent:'space-between',padding:'25px 100px'}}>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/message">message</NavLink>
+        <NavLink to="/bookmark">bookmark</NavLink>
+    </nav>
+  )
+}
